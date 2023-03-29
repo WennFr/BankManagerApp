3ea11d40c1246a2c5228a48e853587847dfc827d
Frederick <frederick.wennborg@gmail.com>
2023-03-29 08:27:53 +0200

    added top customers by country razor page

---
+++ BankManagerApp/Pages/Customers/TopCustomersByCountry.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BankManagerApp.Pages.Customers
+{
+    public class TopCustomersByCountryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
