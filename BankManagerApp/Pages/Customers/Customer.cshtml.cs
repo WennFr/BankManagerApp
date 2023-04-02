969ae56241eee73f1ff072633997569f7b126205
Frederick <frederick.wennborg@gmail.com>
2023-04-02 12:19:23 +0200

    Fixed navigation beetween accounts and customer pages

--- BankManagerApp/Pages/Customers/Customer.cshtml.cs
+++ BankManagerApp/Pages/Customers/Customer.cshtml.cs
@@ -19,16 +19,18 @@ namespace BankManagerApp.Pages.Customers
         public CustomerInformationViewModel Customer { get; set; }
         public List<AccountViewModel> Accounts { get; set; }
         public string PreviousPage { get; set; }
+        public int AccountIdFromAccountPage { get; set; }
         public string Country { get; set; }
 
 
 
-        public void OnGet(int customerId, string previousPage)
+        public void OnGet(int customerId, string previousPage,int accountId)
         {
             Customer = _customerService.GetFullCustomerInformationById(customerId);
             Accounts = _accountService.GetAccountsByCustomerId(customerId).ToList();
             PreviousPage = previousPage;
             Country = Customer.Country;
+            AccountIdFromAccountPage = accountId;
         }
     }
 }
