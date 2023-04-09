bc58e30158457f11fa7c701f19f3aa950e9ed448
Frederick <frederick.wennborg@gmail.com>
2023-04-09 11:44:22 +0200

    fixed basic layout for deposit page

---
+++ BankManagerApp/Pages/Accounts/Deposit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BankManagerApp.Pages.Accounts
+{
+    public class DepositModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
