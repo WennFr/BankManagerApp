6d6ad27369763f5ef29853a7c1efcd6f5bf3077f
Frederick <frederick.wennborg@gmail.com>
2023-04-19 16:24:36 +0200

    changed public on identity service

--- BankRepository/Services/IIdentityUserService.cs
+++ BankRepository/Services/IIdentityUserService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BankRepository.Services
 {
-    internal interface IIdentityUserService
+    public interface IIdentityUserService
     {
     }
 }
--- BankRepository/Services/IdentityUserService.cs
+++ BankRepository/Services/IdentityUserService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BankRepository.Services
 {
-    internal class IdentityUserService
+    public class IdentityUserService
     {
     }
 }
