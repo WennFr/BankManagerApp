28c0f2a498b92cf913fcb49405162f88f46188fe
Frederick <frederick.wennborg@gmail.com>
2023-04-26 11:12:12 +0200

    added builder.services for importing BankAppContext and service interfaces

---
+++ BankWebAPI/Controllers/MeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+using BankRepository.BankAppData;
+using BankRepository.Services.CustomerService;
+
+namespace BankWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MeController : ControllerBase
+    {
+        public MeController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        private readonly ICustomerService _customerService;
+
+
+
+        [HttpGet]
+        [Route("{id}")]
+        //[Authorize(Roles = "Admin, User")]
+        public async Task<ActionResult<Customer>> GetOne(int id)
+        {
+            var customerViewModel = _customerService.GetFullCustomerInformationById(id);
+
+            if (customerViewModel == null)
+            {
+                return BadRequest("Customer not found");
+            }
+            return Ok(customerViewModel);
+        }
+
+
+
+
+    }
+}
--- BankWebAPI/Program.cs
+++ BankWebAPI/Program.cs
@@ -1,11 +1,32 @@
+using BankRepository.Services.AccountService;
+using BankRepository.Services.CustomerService;
+using BankRepository.Services.TransactionService;
+using BankRepository.BankAppData;
+using BankRepository.Data;
+using Microsoft.EntityFrameworkCore;
+using BankRepository.Infrastructure.Profiles;
+
 var builder = WebApplication.CreateBuilder(args);
 
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<BankAppDataContext>(options =>
+    options.UseSqlServer(connectionString));
+
+
 // Add services to the container.
+builder.Services.AddTransient<ICustomerService, CustomerService>();
+builder.Services.AddTransient<IAccountService, AccountService>();
+builder.Services.AddTransient<ITransactionService, TransactionService>();
+builder.Services.AddAutoMapper(typeof(CustomerProfile).Assembly);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+
 
 var app = builder.Build();
 
