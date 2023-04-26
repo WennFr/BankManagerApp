2d1ba0bf6c8d357828bf19f356d214c842021f03
Frederick <frederick.wennborg@gmail.com>
2023-04-26 14:25:28 +0200

    refactor

--- BankWebAPI/User/UserCredentials.cs
+++ BankWebAPI/User/UserCredentials.cs
@@ -1,18 +1,30 @@
-﻿namespace BankWebAPI.User
+﻿using BankRepository.Services.CustomerService;
+
+namespace BankWebAPI.User
 {
     public class UserCredentials
     {
+        private readonly ICustomerService _customerService;
+
+        public UserCredentials(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+
+
         public static List<UserModel> Users = new List<UserModel>()
         {
-            new UserModel() // Full read/write access
+            new UserModel() // Can only Read
             {
-                UserName = "richard_admin",
-                EmailAddress = "richard_admin@email.se",
-                Password = "passwordAdmin",
+                UserName = "richard_user",
+                EmailAddress = "richard_user@email.se",
+                Password = "passwordUser",
                 GivenName = "Richard",
-                SurName = "chalk",
+                SurName = "Chalk",
                 Role = "User",
-            },
+            }
+
         };
     }
 
