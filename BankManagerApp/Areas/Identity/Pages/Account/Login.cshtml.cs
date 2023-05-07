231f4c35bac0f873285bd19c315dbb7f3948284d
Frederick <frederick.wennborg@gmail.com>
2023-05-07 21:27:58 +0200

    Fixed so that logout page is not reached after login a new user

--- BankManagerApp/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ BankManagerApp/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -103,7 +103,9 @@ namespace BankManagerApp.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl ??= Url.Content("~/");
+            //returnUrl ??= Url.Content("~/");
+
+            returnUrl = Url.Content("~/");
 
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
 
